aa3fa4c29f522b3883f676bedcf882e0fac50595
JeffM <jeff@jeffreymclean.com>
2019-09-04 18:42:30 -0700

    Added reset.css with MeyerWeb. Added Psuedo Code to app.js. Built out basic HTML structur ein index.html

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+/* Psuedo Code - Trivia Game
+
+Page/Site Loads
+
+Main content area shows Title and Start Button. 
+    
+    When Start Button is Clicked
+        - Area where button was is cleared
+        - Timer Appears at the top of the page & starts counting down
+        - Questions are Loaded to the Page w/ Answers underneath as Radio Buttons.
+        - Done Button is loaded to the bottom of the page 
+
+    While answering questions:
+        - Correct Answers are Recorded
+        - Wrong Answers are Recorded
+        - Unanswered questions are Recorded
+
+    If Timer Hits Zero:
+        - Questions & Done button are cleared
+        - Displays "All Done" Message
+        - Shows Correct, Wrong, and Unanswered count 
+
+    If Done Button is Clicked:
+        - Questions & Done button are cleared
+        - Display "All Done" Message
+        - Show Correct, Wrong, and Unanswered count. 
+
